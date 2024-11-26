095b5e95bc4cccd1c118e427d7e0ae7424f403d4
Rado <rj.airoo@gmail.com>
2024-11-26 12:38:39 +0100

    events

--- 03/src/App.jsx
+++ 03/src/App.jsx
@@ -1,13 +1,18 @@
 function App() {
-	const buttonEl = <button>Pokaż spoiler...</button>
-  console.log(buttonEl);
+	function handleClick() {
+		alert("Klik!")
+	}
+	
 	return (
 		<>
 			<h1>Gwiezdne Wojny V</h1>
 			<h2>Rok produkcji: 1980 </h2>
 			<h2>Fabuła:</h2>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-			{buttonEl}
+			<button
+				onClick={handleClick}>
+				Pokaż spoiler...
+			</button>
 			<p>Lorem ipsum dolor sit amet.</p>
 		</>
 	)
