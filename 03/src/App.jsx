94772cfff6064a2e49cb6ab2ff0ff70bc0154bbe
Rado <rj.airoo@gmail.com>
2024-11-26 17:16:13 +0100

    state

--- 03/src/App.jsx
+++ 03/src/App.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
+
 function App() {
-	function handleClick() {
-		alert("Klik!")
+	const [isSpoilershown, setIsSpoilerShown] = useState(false)
+	const [isWarningshown, setIsWarningshown] = useState(true)
+
+	function handleShownSpoilerClick() {
+		setIsSpoilerShown(true)
+		console.log('function handleClick')
+		setIsWarningshown(false)
 	}
-	
+
+	function handleCloseWarningClick() {
+		setIsWarningshown(false)
+	}
+	console.log('App render')
+
 	return (
 		<>
 			<h1>Gwiezdne Wojny V</h1>
 			<h2>Rok produkcji: 1980 </h2>
 			<h2>Fabuła:</h2>
+			{isWarningshown && (
+				<p>
+					Uwaga! Zawiera spoilery <button onClick={handleCloseWarningClick}>X</button>
+				</p>
+			)}
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-			<button
-				onClick={handleClick}>
-				Pokaż spoiler...
-			</button>
-			<p>Lorem ipsum dolor sit amet.</p>
+			{isSpoilershown ? (
+				<p>Lorem ipsum dolor sit amet.</p>
+			) : (
+				<button onClick={handleShownSpoilerClick}>Pokaż spoiler...</button>
+			)}
 		</>
 	)
 }
