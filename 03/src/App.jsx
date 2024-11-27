24023dd4b5423a9de1565ba1700ede9e87b1994e
Rado <rj.airoo@gmail.com>
2024-11-27 13:16:05 +0100

    logic grouping

--- 03/src/App.jsx
+++ 03/src/App.jsx
@@ -1,36 +1,17 @@
-import { useState } from 'react'
+import { LikesCounter } from './components/LikesCounter'
+import { Plot } from './components/Plot'
 
 function App() {
-	const [isSpoilershown, setIsSpoilerShown] = useState(false)
-	const [isWarningshown, setIsWarningshown] = useState(true)
 
-	function handleShownSpoilerClick() {
-		setIsSpoilerShown(true)
-		console.log('function handleClick')
-		setIsWarningshown(false)
-	}
 
-	function handleCloseWarningClick() {
-		setIsWarningshown(false)
-	}
-	console.log('App render')
+	console.log('<App> - render')
 
 	return (
 		<>
-			<h1>Gwiezdne Wojny V</h1>
-			<h2>Rok produkcji: 1980 </h2>
-			<h2>Fabuła:</h2>
-			{isWarningshown && (
-				<p>
-					Uwaga! Zawiera spoilery <button onClick={handleCloseWarningClick}>X</button>
-				</p>
-			)}
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-			{isSpoilershown ? (
-				<p>Lorem ipsum dolor sit amet.</p>
-			) : (
-				<button onClick={handleShownSpoilerClick}>Pokaż spoiler...</button>
-			)}
+			<h1>Gwiezdne wojny V</h1>
+			<h2>Rok produkcji: 1980</h2>
+			<LikesCounter />
+			<Plot/>
 		</>
 	)
 }
---
+++ 03/src/components/LikesCounter.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export function LikesCounter() {
+	const [numberOfLikes, setNumberOfLikes] = useState(50)
+
+	function handleLikeButtonClick() {
+		setNumberOfLikes(previousNumberOfLikes => previousNumberOfLikes + 1)
+	}
+
+	function handleLoveButtonClick() {
+		setNumberOfLikes(previousNumberOfLikes => previousNumberOfLikes + 3)
+	}
+
+	return (
+		<>
+			<h2>Liczba polubień: {numberOfLikes}</h2>
+			<button onClick={handleLikeButtonClick}>👍</button>
+			<button onClick={handleLoveButtonClick}>❤️</button>
+		</>
+	)
+}
